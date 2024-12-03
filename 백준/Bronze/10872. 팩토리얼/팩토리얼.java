4d0083a61c1cfe131cd96271cd36ad39efae5bad
MurHyun2 <eddy152264@gmail.com>
2024-12-03 10:49:35 +0900

    [Bronze III] Title: 팩토리얼, Time: 64 ms, Memory: 11464 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static int T;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		T = Integer.parseInt(br.readLine());
+
+		System.out.println(cal(T));
+	}
+
+	private static int cal(int t) {
+		if (t == 0)
+			return 1;
+
+		return t * cal(t - 1);
+	}
+
+}
