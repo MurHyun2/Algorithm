c4f10065dcd60b55cded0273f33d673d739f3feb
MurHyun2 <eddy152264@gmail.com>
2024-12-03 10:51:05 +0900

    [Bronze I] Title: 컵홀더, Time: 80 ms, Memory: 11436 KB -BaekjoonHub

---
+++ 백준/Bronze/2810. 컵홀더/컵홀더.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+		String input = br.readLine();
+
+		int ch = 1;
+
+		for (int i = 0; i < T; i++) {
+			if (input.charAt(i) == 'S') {
+				ch++;
+			} else {
+				ch++;
+				i++;
+			}
+		}
+
+		if (ch > T)
+			ch = T;
+
+		System.out.println(ch);
+	}
+}
