1f77a4b57e635b3c9eb95dc8993a93987c60d2aa
MurHyun2 <eddy152264@gmail.com>
2024-12-03 10:48:26 +0900

    [Gold IV] Title: N-Queen, Time: 6460 ms, Memory: 12184 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static int N, cnt;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine().trim());
+		arr = new int[N];
+		cnt = 0;
+
+		dfs(0);
+
+		System.out.println(cnt);
+	}
+
+	private static void dfs(int col) {
+		if (col == N) {
+			cnt++;
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			arr[col] = i;
+			if (check(col)) {
+				dfs(col + 1);
+			}
+		}
+	}
+
+	private static boolean check(int col) {
+
+		for (int i = 0; i < col; i++) {
+			if (arr[col] == arr[i])
+				return false;
+
+			if (Math.abs(col - i) == Math.abs(arr[col] - arr[i]))
+				return false;
+		}
+
+		return true;
+	}
+}
