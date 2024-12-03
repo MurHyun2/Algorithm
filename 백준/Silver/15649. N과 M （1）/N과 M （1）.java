6e9b30ba161540195a84ef0f8908df87e1d52376
MurHyun2 <eddy152264@gmail.com>
2024-12-03 10:49:19 +0900

    [Silver III] Title: N과 M (1), Time: 156 ms, Memory: 20496 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] arr, sel;
+	static boolean[] visited;
+	static BufferedWriter bw;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		sel = new int[M];
+		visited = new boolean[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = i + 1;
+		}
+
+		perm(0);
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+	private static void perm(int depth) throws IOException {
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(sel[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				sel[depth] = arr[i];
+				perm(depth + 1);
+				visited[i] = false;
+			}
+		}
+	}
+}
