ef656ef1e0599336cbb8a5b326a82236c8df5502
MurHyun2 <eddy152264@gmail.com>
2024-12-03 10:48:32 +0900

    [Bronze I] Title: 암호 키, Time: 156 ms, Memory: 11752 KB -BaekjoonHub

---
+++ 백준/Bronze/1816. 암호 키/암호 키.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine().trim());
+
+		for (int tc = 0; tc < N; tc++) {
+			long num = Long.parseLong(br.readLine().trim());
+			String result = "YES";
+
+			for (int i = 2; i <= 1_000_000; i++) {
+				if (num % i == 0) {
+					result = "NO";
+					break;
+				}
+			}
+
+			System.out.println(result);
+		}
+	}
+}
