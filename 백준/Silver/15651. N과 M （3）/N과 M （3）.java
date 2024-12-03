9413d89b10c292110b93e8488cf6f1b990142d43
MurHyun2 <eddy152264@gmail.com>
2024-12-03 10:49:11 +0900

    [Silver III] Title: N과 M (3), Time: 340 ms, Memory: 112432 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] arr, sel;
+	static BufferedWriter bw;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		sel = new int[M];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = i + 1;
+		}
+
+		perm(0);
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+	private static void perm(int depth) {
+		if(depth == M) {
+			for (int i : sel) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			sel[depth] = arr[i];
+			perm(depth + 1);
+		}
+	}
+}
