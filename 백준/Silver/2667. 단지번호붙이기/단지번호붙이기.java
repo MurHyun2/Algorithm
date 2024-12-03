e3e2571251716bd627f714eca2658ac38116b0e0
MurHyun2 <eddy152264@gmail.com>
2024-12-03 10:49:44 +0900

    [Silver I] Title: 단지번호붙이기, Time: 80 ms, Memory: 11660 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+	static int N, cnt;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dr = { 1, -1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N];
+
+		for (int r = 0; r < N; r++) {
+			String str = br.readLine().trim();
+
+			for (int c = 0; c < N; c++) {
+				map[r][c] = str.charAt(c) - '0';
+			}
+		}
+
+		ArrayList<Integer> arr = new ArrayList<>();
+
+		int result = 0;
+
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < N; c++) {
+				cnt = 0;
+				if (map[r][c] == 1) {
+					dfs(r, c);
+					arr.add(cnt);
+					result++;
+				}
+			}
+		}
+
+		Collections.sort(arr);
+
+		System.out.println(result);
+
+		for (int num : arr) {
+			System.out.println(num);
+		}
+
+	}
+
+	private static void dfs(int r, int c) {
+		map[r][c] = 0;
+		cnt += 1;
+
+		for (int i = 0; i < 4; i++) {
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+
+			if (nr >= 0 && nc >= 0 && nr < N && nc < N && map[nr][nc] == 1) {
+				dfs(nr, nc);
+			}
+		}
+	}
+}
