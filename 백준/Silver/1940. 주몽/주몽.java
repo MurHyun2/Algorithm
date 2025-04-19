f06fdd7cf7cbd9b2f6185575841d176ee439bf5e
Daehyun Lee <eddy152264@gmail.com>
2025-04-20 00:51:47 +0900

    [Silver IV] Title: 주몽, Time: 172 ms, Memory: 16436 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,15 +1,15 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-        boolean[] usedMaterials = new boolean[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
@@ -17,16 +17,22 @@ public class Main {
         }
 
         int cnt = 0;
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (!usedMaterials[i] && !usedMaterials[j]) {
-                    if(arr[i] + arr[j] == M) {
-                        usedMaterials[i] = true;
-                        usedMaterials[j] = true;
-                        cnt++;
-                        break;
-                    }
-                }
+        int left = 0;
+        int right = N - 1;
+
+        Arrays.sort(arr);
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == M) {
+                cnt++;
+                left++;
+                right--;
+            } else if (sum < M) {
+                left++;
+            } else {
+                right--;
             }
         }
 
