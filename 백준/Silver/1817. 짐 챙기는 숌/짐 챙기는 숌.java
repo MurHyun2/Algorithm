d0d0ba8a287d0b85faa7eacf0c5a2169cf262814
Daehyun Lee <eddy152264@gmail.com>
2025-05-08 21:47:07 +0900

    [Silver V] Title: 짐 챙기는 숌, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Silver/1817. 짐 챙기는 숌/짐 챙기는 숌.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+        
+		if (N > 0) {
+			st = new StringTokenizer(br.readLine());
+            
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int cnt = N > 0 ? 1 : 0;
+		int sum = 0;
+		
+        
+		for (int i : arr) {
+			sum += i;
+            
+			if (sum > M) {
+				cnt++;
+				sum = i;
+			}
+		}
+        
+		System.out.println(cnt);
+	}
+}
