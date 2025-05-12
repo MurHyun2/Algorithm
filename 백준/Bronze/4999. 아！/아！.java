e0c233cc1a25d2b978364c7b9b05895f365cfd3e
Daehyun Lee <eddy152264@gmail.com>
2025-05-12 21:15:35 +0900

    [Bronze V] Title: 아!, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        if (br.readLine().length() >= br.readLine().length()) {
+            System.out.println("go");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
