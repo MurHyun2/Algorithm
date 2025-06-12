7a169976b070acc5eb25137ff817e54e9ece2140
Daehyun Lee <eddy152264@gmail.com>
2025-06-12 17:07:14 +0900

    [level 2] Title: 올바른 괄호, Time: 19.32 ms, Memory: 55.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            }
+            
+            if (c == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        
+        if (!stack.isEmpty()) {
+            answer = false;
+        }
+        
+        return answer;
+    }
+}
