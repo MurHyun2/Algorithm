6c77974686f8bc8a1089e32f1b6db48715ee3ca0
Lee Daehyun <eddy152264@gmail.com>
2025-09-29 21:55:34 +0900

    [Bronze V] Title: Cupcake Party, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int r = Integer.parseInt(br.readLine());
+        int s = Integer.parseInt(br.readLine());
+        
+        System.out.println((r * 8) + (s * 3) -28);
+    }
+}
