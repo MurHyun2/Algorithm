d77162c3721657d5301242026b1fd76bce97c2dd
Lee Daehyun <eddy152264@gmail.com>
2025-10-03 21:40:33 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 136 ms, Memory: 16356 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+        
+        System.out.print(n.divide(m) + "\n" + n.remainder(m));
+    }
+}
