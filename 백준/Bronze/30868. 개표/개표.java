ed8bad82bd0315bbb07b3029b43de566f503e421
Lee Daehyun <eddy152264@gmail.com>
2025-10-12 21:36:04 +0900

    [Bronze IV] Title: 개표, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/30868. 개표/개표.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            StringBuilder sb = new StringBuilder();
+            
+            while (n > 0) {
+                if (n >= 5) {
+                    n -= 5;
+                    sb.append("++++ ");
+                } else {
+                    for (int j = 0; j < n; j++) {
+                        sb.append("|");
+                    }
+                    
+                    n -= n;
+                }
+            }
+            
+            System.out.println(sb);
+        }
+    }
+}
