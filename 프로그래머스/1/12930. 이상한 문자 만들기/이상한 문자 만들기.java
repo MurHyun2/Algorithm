d2df044cc865663b6ed4b30216813732e7ccefc7
Lee Daehyun <eddy152264@gmail.com>
2025-12-20 11:47:07 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.13 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        int idx = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (ch == ' ') { 
+                sb.append(' ');
+                idx = 0;
+            } else {
+                if (idx % 2 == 0) {
+                    sb.append(Character.toUpperCase(ch));
+                } else {
+                    sb.append(Character.toLowerCase(ch));
+                }
+                idx++;
+            }
+        }
+
+        return sb.toString();
+    }
+}
