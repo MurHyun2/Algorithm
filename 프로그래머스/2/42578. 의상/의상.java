b54c9291d3090c99f30e36869c016582a661f72d
Lee Daehyun <eddy152264@gmail.com>
2026-03-03 19:59:30 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String[] str : clothes) {
+            String type = str[1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
+        }
+        
+        int answer = 1;
+        
+        for (int i : map.values()) {
+            answer *= (i + 1);
+        }
+         
+        return answer - 1;
+    }
+}
