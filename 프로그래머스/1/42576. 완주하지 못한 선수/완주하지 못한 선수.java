4c68e392bb232c9669cccb1e20b493ef7578c713
Lee Daehyun <eddy152264@gmail.com>
2026-03-04 23:39:32 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 77.54 ms, Memory: 97.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String runner : completion) {
+            map.put(runner, map.getOrDefault(runner, 0) + 1);
+        }
+        
+        for (String runner : participant) {
+            int cnt = map.getOrDefault(runner, 0);
+            if (cnt == 0) return runner;
+            map.put(runner, map.get(runner) - 1);
+        }
+        
+        return "";
+    }
+}
