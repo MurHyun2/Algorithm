460f0de671310a3ae7b72c8f0b66d6244a7c2bb1
Lee Daehyun <eddy152264@gmail.com>
2026-03-23 21:36:57 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 100 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            String name = st.nextToken();
+            int age = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            if (name.equals("#") && age == 0 && weight == 0) {
+                break;
+            }
+
+            if (age > 17 || weight >= 80) {
+                sb.append(name).append(" Senior\n");
+            } else {
+                sb.append(name).append(" Junior\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
