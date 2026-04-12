464ba62f179df965686cd0fc2c856789d417397b
Lee Daehyun <eddy152264@gmail.com>
2026-04-12 13:34:19 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.04 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,67 +1,29 @@
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        int castingTime = bandage[0];
-        int healingPerSec = bandage[1];
-        int bonusHeal = bandage[2];
-        
-        int endTime = attacks[attacks.length - 1][0];
-        int[] attack = new int[endTime + 1];
-        
-        for (int[] i : attacks) {
-            int time = i[0];
-            int damage = i[1];
-            attack[time] = damage;
+        int castTime = bandage[0];
+        int healPerSec = bandage[1];
+        int extraHeal = bandage[2];
+
+        int maxHp = health;
+        int curHp = health;
+        int lastAtkTime = 0;
+
+        for (int[] attack : attacks) {
+            int atkTime = attack[0];
+            int dmg = attack[1];
+
+            int healTime = atkTime - lastAtkTime - 1;
+
+            curHp += healTime * healPerSec;
+            curHp += (healTime / castTime) * extraHeal;
+            if (curHp > maxHp) curHp = maxHp;
+
+            curHp -= dmg;
+            if (curHp <= 0) return -1;
+
+            lastAtkTime = atkTime;
         }
-        
-        int casting = 0;
-        int hp = health;
-        
-        for (int i = 1; i <= endTime; i++) {
-            int damage = attack[i];
-            
-            if (damage != 0) {
-                hp -= damage;
-                
-                if (hp < 1) return -1;
-                
-                casting = 0;
-                continue;
-            }
-            
-            hp += healingPerSec;
-            casting++;
-            
-            if (casting == castingTime) {
-                hp += bonusHeal;
-                casting = 0;
-            }
-            
-            if (hp > health) hp = health;
-        }
-        
-        return hp;
+
+        return curHp;
     }
 }
-
-/*
-attacks 이 끝나면 종료
-for ( 0 ~ attacks[attacks.length - 1][0]) {
-    if (공격 체크) {
-        체력 까기
-        시간 초기화
-        0 이면 -1 리턴
-    }
-
-        체력 + 초당 회복
-        시전시간 체크++
-    
-    if (시전시간) {
-        체력 회복
-    }
-    
-    if (최대 체력 넘으면) {
-        최대 체력까지만
-    }
-}
-현재 체력 리턴
-*/
