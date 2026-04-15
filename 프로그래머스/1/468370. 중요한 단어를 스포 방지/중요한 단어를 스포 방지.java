2502e6bb49eaebf1fff438d40705321c684615d1
Lee Daehyun <eddy152264@gmail.com>
2026-04-15 15:51:11 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 1.69 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
@@ -2,60 +2,47 @@ import java.util.*;
 
 class Solution {
     public int solution(String message, int[][] spoiler_ranges) {
-        char[] messageWithSpoiler = message.toCharArray();
-        int ml = message.length();
-        StringBuilder sb = new StringBuilder();
+        int n = message.length();
+        boolean[] hidden = new boolean[n];
         
-        
-        for (int[] spoiler_range : spoiler_ranges) {
-            int start = spoiler_range[0];
-            int end = spoiler_range[1];
-            
-            for (int i = start; i <= end; i++) {
-                if (messageWithSpoiler[i] == ' ') continue;
-                
-                messageWithSpoiler[i] = '#';
+        for (int[] range : spoiler_ranges) {
+            for (int i = range[0]; i <= range[1]; i++) {
+                hidden[i] = true;
             }
         }
         
-        for (char ch : messageWithSpoiler) {
-            sb.append(ch);
-        }
+        Set<String> seenWords = new HashSet<>();
+        List<String> hiddenWords = new ArrayList<>();
         
-        String spoilerMessage = sb.toString();
-        String[] words = spoilerMessage.split(" ");
-        Set<String> wordSet = new HashSet<>();
-        
-        for (String word : words) {
-            wordSet.add(word);
+        int start = 0;
+        while (start < n) {
+            int end = start;
+            boolean isSpoiler = false;
+            
+            while (end < n && message.charAt(end) != ' ') {
+                if (hidden[end]) {
+                    isSpoiler = true;
+                }
+                end++;
+            }
+            
+            String word = message.substring(start, end);
+            
+            if (isSpoiler) {
+                hiddenWords.add(word);
+            } else {
+                seenWords.add(word);
+            }
+            
+            start = end + 1;
         }
         
         int answer = 0;
-        sb = new StringBuilder();
-        boolean isSpoiler = false;
         
-        for (int i = 0; i < ml; i++) {
-            char ch = spoilerMessage.charAt(i);
-            
-            if (ch == '#') {
-                sb.append(message.charAt(i));
-                isSpoiler = true;
-            } else if (ch != ' ') {
-                sb.append(message.charAt(i));
-            }
-            
-            if (i == ml - 1 || message.charAt(i) == ' ') {
-                if (isSpoiler) {
-                    String word = sb.toString();
-                    isSpoiler = false;
-
-                    if (!wordSet.contains(word)) {
-                        wordSet.add(word);
-                        answer++;
-                    }
-                }
-                
-                sb = new StringBuilder();
+        for (String word : hiddenWords) {
+            if (!seenWords.contains(word)) {
+                seenWords.add(word);
+                answer++;
             }
         }
         
@@ -72,6 +59,9 @@ class Solution {
 3. 중복 x
 4. 여러 단어가 동시 공개 된 경우 왼쪽부터 순서대로 체크(앞에서 등장했는데 뒤에 단어가 스포 방지 처리 되었으면 유일한 단어 처리함)
 
-단어 배열 만들어서 단어들 스포 방지 포함해서 넣기
-스포 방지 단어들 하나씩 체크하면서 존재 여부 + 중복 체크
+스포일러인 부분들을 체크해서 boolean으로 idx 관리
+공백이나 마지막전까지 idx를 올려가며 추가
+스포일러인 부분이 포함 되었다면 스포일러 리스트에 추가
+공백이나 마지막이라면 set에 넣어서 중복 관리
+스포일러 리스트들을 순회하면서 set에 중복 체크 후 중복이 아니라면 answer++
 */
