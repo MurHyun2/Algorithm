7d5b3363ab260193c49d8328644a6565fd836da7
Lee Daehyun <eddy152264@gmail.com>
2026-04-16 11:51:39 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 105.05 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -2,17 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] elements) {
-        int n = elements.length;
         Set<Integer> sumSet = new HashSet<>();
+        int n = elements.length;
+        int[] arr = new int[2 * n];
+        for (int i = 0; i < 2 * n; i++) {
+            arr[i] = elements[i % n];
+        }
+        
+        int[] prefix = new int[2 * n + 1];
+        for (int i = 0; i < 2 * n; i++) {
+            prefix[i + 1] = prefix[i] + elements[i % n];
+        }
         
         for (int len = 1; len <= n; len++) {
             for (int start = 0; start < n; start++) {
-                int sum = 0;
-                
-                for (int k = 0; k < len; k++) {
-                    sum += elements[(start + k) % n];
-                }
-                
+                int sum = prefix[start + len] - prefix[start];
                 sumSet.add(sum);
             }
         }
@@ -22,5 +26,5 @@ class Solution {
 }
 
 /*
-완탐으로 모든 수를 돌리면서 set에 합을 담고 갯수를 리턴
+0 7 16 17 18 22 29 38 39 40 44
 */
