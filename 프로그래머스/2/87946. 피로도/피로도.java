3debf2ef7a1c55a2c63c619b42b7c63f690b7679
Lee Daehyun <eddy152264@gmail.com>
2026-04-16 10:23:50 +0900

    [level 2] Title: 피로도, Time: 5.53 ms, Memory: 93.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    int max = 0;
+    boolean[] visited;
+    
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(k, dungeons, 0);
+        return max;
+    }
+    
+    void dfs(int fatigue, int[][] dungeons, int cnt) {
+        max = Math.max(max, cnt);
+        
+        for (int i = 0; i < dungeons.length; i++) {
+            int need = dungeons[i][0];
+            int use = dungeons[i][1];
+            
+            if (fatigue >= need && !visited[i]) {
+                visited[i] = true;
+                dfs(fatigue - use, dungeons, cnt + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
+
+
+/*
+최소 피로도 -> 입장을 위한 피로도, 소모 피로도 -> 던전 탐험 후 소모 피로도
+각 던전은 하루에 한번만, 최대한 많은 던전 탐험
+
+각 던전들을 완탐으로 풀고 최댓값 저장
+
+[[80,80],[79,1],[78,1],[77,77],[76,1],[75,75]]
+79 78 76 75
+*/
