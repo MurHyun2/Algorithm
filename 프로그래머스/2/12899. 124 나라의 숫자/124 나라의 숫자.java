bd0b5fc6d1da6cf6dc172835899402b6b674a977
Lee Daehyun <eddy152264@gmail.com>
2026-05-27 21:31:04 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.03 ms, Memory: 55.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        String[] nums = {"1", "2", "4"};
+        
+        while (n > 0) {
+            n--;
+            sb.append(nums[n % 3]);
+            n /= 3;
+        }
+        
+        return sb.reverse().toString();
+    }
+}
