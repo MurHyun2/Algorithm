fc769a9d3cd3a5b81fb3c4d990045f6925caaff7
Lee Daehyun <eddy152264@gmail.com>
2026-05-31 16:27:05 +0900

    [level 1] Title: K번째수, Time: 0.26 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,22 +1,18 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
         for (int i = 0; i < commands.length; i++) {
-            int start = commands[i][0];
+            int start = commands[i][0] - 1;
             int end = commands[i][1];
-            int[] arr = new int[end - start + 1];
-            int idx = 0;
+            int pick = commands[i][2] - 1;
             
-            for (int j = start - 1; j <= end - 1; j++) {
-                arr[idx++] = array[j];
-            }
-            
+            int[] arr = Arrays.copyOfRange(array, start, end);
             Arrays.sort(arr);
-            answer[i] = arr[commands[i][2] - 1];
+            
+            answer[i] = arr[pick];
         }
         
         return answer;
