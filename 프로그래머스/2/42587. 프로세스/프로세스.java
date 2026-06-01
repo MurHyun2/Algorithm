c70cd60407a19dfad44ca0fe3ecf2b6b520bf451
Lee Daehyun <eddy152264@gmail.com>
2026-06-01 11:58:14 +0900

    [level 2] Title: 프로세스, Time: 0.68 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,28 +2,28 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        Deque<int[]> queue = new LinkedList<>();
+        Queue<Integer> queue = new ArrayDeque<>();
         int[] count = new int[10];
-        int max = Integer.MIN_VALUE;
+        int max = 0;
         
         for (int i = 0; i < priorities.length; i++) {
             int priority = priorities[i];
-            queue.offer(new int[] {priority, i});
+            queue.offer(i);
             count[priority]++;
             max = Math.max(max, priority);
         }
         
         int order = 0;
+
         while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int priority = current[0];
-            int index = current[1];
+            int cur = queue.poll();
+            int priority = priorities[cur];
             
             if (priority == max) {
-                order++;
                 count[priority]--;
+                order++;
                 
-                if (index == location) {
+                if (cur == location) {
                     return order;
                 }
                 
@@ -31,7 +31,7 @@ class Solution {
                     max--;
                 }
             } else {
-                queue.offer(current);
+                queue.offer(cur);
             }
         }
         
@@ -39,7 +39,3 @@ class Solution {
     }
 }
 
-/**
-큐에서 꺼낸뒤 나머지와 비교해서 더 높은 순위가 있다면 다시 큐에 넣기
-큐에 원래 순서도 같이 저장해서 더 높은 우선순위가 없고 순서가 맞다면 리턴
-*/
