881de7a093f07ba7552ee99959804316a241a508
Lee Daehyun <eddy152264@gmail.com>
2026-06-02 17:31:40 +0900

    [level 2] Title: 성분으로 구분한 아이스크림 총 주문량, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
@@ -0,0 +1,13 @@
+SELECT I.INGREDIENT_TYPE,
+       SUM(F.TOTAL_ORDER) AS TOTAL_ORDER
+FROM ICECREAM_INFO I
+JOIN FIRST_HALF F
+  ON I.FLAVOR = F.FLAVOR
+GROUP BY I.INGREDIENT_TYPE
+ORDER BY SUM(F.TOTAL_ORDER) ASC;
+
+/*
+상반기 동안 각 아이스크림 성분 타입과 성분 타입에 대한 아이스크림의 총주문량을
+총주문량이 작은 순서대로 조회하는 SQL 문을 작성해주세요.
+이때 총주문량을 나타내는 컬럼명은 TOTAL_ORDER로 지정해주세요.
+*/
